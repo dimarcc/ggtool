f5a76982a01b9b3218fed568b000105f7ad1482b
dimarcc <dimarcc@clarkson.edu>
2017-08-09 14:44:31 -0400

    Initial COMMIT

---
+++ app/routes/input.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash (
+      {
+        psus: this.store.findAll('psu'),
+        ldrs: this.store.findAll('ldr'),
+        jmps: this.store.findAll('jmp'),
+        wpxes: this.store.findAll('wpx')
+      }
+    )
+  }
+});
